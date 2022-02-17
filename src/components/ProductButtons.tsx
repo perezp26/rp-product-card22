d2f7337f32f5da7c95d6776766a161967c17bdbd
raul perez <perezp@hotmail.com>
2022-02-16 19:21:17 -0600

    Vresion 1.0.0-rc lista

--- src/components/ProductButtons.tsx
+++ src/components/ProductButtons.tsx
@@ -32,7 +32,7 @@ export const ProductsButtons = ({ className, style } : Props )  =>{
                         onClick = { () => increaseBy(-1) }> - </button>
                     <div className={ styles.countLabel }> { counter } </div>
                     <button 
-                        className={ `${styles.buttonAdd} ${ isMaxReached() && styles.disable}` }
+                        className={ `${styles.buttonAdd} ${ isMaxReached() && styles.disabled}` }
                         onClick = { () => increaseBy(1) }> + </button>
         </div>
     )
